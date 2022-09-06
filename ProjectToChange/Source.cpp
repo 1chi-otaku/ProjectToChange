b7849e63e0d58af3aa7f517e0dac57eda28a09f3
Serhii <mywaifuqueen@gmail.com>
2022-09-06 11:56:01 +0100

    Class Point добавлены конструкторы

--- ProjectToChange/Source.cpp
+++ ProjectToChange/Source.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 class Point {
 	
+	int a, b, c;
+public:
+	Point() {
+		a = b =c = 0;
 
+	}
+	Point(int a, int b, int c) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
 };
 class Person {
 	string name;
